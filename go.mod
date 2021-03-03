95dffd3abf21874823611f27af42f8a50ebac305
小小帅 <yew1eb@zhouxiaoshuaideMacBook-Pro.local>
2021-03-03 15:29:09 +0800

    upload

--- go.mod
+++ go.mod
@@ -5,9 +5,11 @@ go 1.13
 require (
 	github.com/buger/jsonparser v1.0.0
 	github.com/djherbis/buffer v1.1.0
+	github.com/go-kratos/kratos v1.0.0 // indirect
 	github.com/google/gops v0.3.11
 	github.com/nightlyone/lockfile v1.0.0
 	github.com/robaho/keydb v0.0.0-20200219155537-3a775e464c52
 	github.com/stackimpact/stackimpact-go v2.3.10+incompatible
 	github.com/tidwall/gjson v1.6.0
+	golang.org/x/sys v0.0.0-20210301091718-77cc2087c03b // indirect
 )
--- string_copy.go
+++ string_copy.go
@@ -1,5 +1,60 @@
 package main
 
+import (
+	"fmt"
+	"net/url"
+	"strings"
+)
+
 func main() {
 
+	url, _  := url.Parse("10.234.123")
+	fmt.Println(url)
+
+	Metric := "ck.query"
+	//Metric := "report.query"
+	if strings.Index(Metric, "report.") < 0 && strings.Index(Metric, "statistics.") < 0 && {
+		fmt.Println("what?")
+		fmt.Println(strings.Index(Metric, "report."))
+		fmt.Println(strings.Index(Metric, "statistics."))
+	}
+	nums := []int {1, 2}
+	permute(nums)
+}
+
+var result [][]int
+
+func permute(nums []int) [][]int {
+	n := len(nums)
+	result = [][]int{}
+	if n == 0 {
+		return result
+	}
+
+	path := make([]int, n)
+	visited := make([]bool, n)
+	backtrack(nums, n, 0, path, visited)
+	return result
+}
+
+func backtrack(nums []int, n, depth int, path []int, visited []bool) {
+	if depth == n {
+		temp := make([]int, n)
+		copy(temp, path)
+		fmt.Println(" path[last]=>", path[len(path) - 1])
+		fmt.Println(" temp[last]=>", temp[len(temp) - 1])
+		result = append(result, temp)
+		return
+	}
+
+	for i := 0; i < n; i++ {
+		if visited[i] {
+			continue
+		}
+		path = append(path, nums[i])
+		visited[i] = true
+		backtrack(nums, n, depth + 1, path, visited)
+		visited[i] = false
+		path = path[:len(path) - 1]
+	}
 }
